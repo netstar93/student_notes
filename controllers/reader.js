f89fbc2e7b839337499d0af671dccc9d84ed04f3
Nick <nickdev@gmail.com>
2018-11-28 19:40:36 +0530

    2nd day

--- controllers/reader.js
+++ controllers/reader.js
@@ -4,12 +4,25 @@ var request = require('request');
 var Notes = require('../models/notes');
 var apiHelper = require('../helpers/news-api');
 var fs = require('fs');
+var Parser = require('rss-parser');
+let parser = new Parser(); 
+var top_headlines = "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=0289bc2596de42329baf44ae460f1bc4";
 
-router.get('/' , function(req,res){
- apiHelper.getNews(function(news){
-     res.render('newslist' , {title : 'New Note' , 'newsData' : news } );
-       // console.log(response);
-    });
+router.get('/' , function(req,response){
+    // app.set('source', req.query.source);
+
+        var data = {};
+        var feed_url = getFeedUrl( req.query.source);
+        let feed = parser.parseURL('https://www.reddit.com/.rss');
+        request.get(feed_url, function (err, res,body) {
+            data = JSON.parse(body);             
+            if(data.status == "ok")
+            response.render('newslist' , {title : 'New Note' , 'newsData' : data } );            
+        })
 })
+
+function getFeedUrl(source_name){
+    return top_headlines;
+}
 
 module.exports = router;
--- helpers/news-api.js
+++ helpers/news-api.js
@@ -1,17 +1,25 @@
 var express  = require('express');
 var router = express.Router();
 var request = require("request");
-const top_headlines = "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=0289bc2596de42329baf44ae460f1bc4";
+var Parser = require('rss-parser');
+let parser = new Parser();  
+var top_headlines = "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=0289bc2596de42329baf44ae460f1bc4";
 
 module.exports = {
-     getNews : function (callback) {
+     getNews : function (callback,reqObj) {
          var data = {};
+         var feed_url = getFeedUrl(req);
+         console.log(reqObj); 
+         let feed = parser.parseURL('https://www.reddit.com/.rss');
         request.get(top_headlines, function (err, res,body) {
-             data = JSON.parse(body);
-             // if(data.status == "ok")
-             return callback(JSON.parse(body));
-            // console.log(JSON.parse(body));
+             data = JSON.parse(body);             
+             if(data.status == "ok")
+             return callback(data);
+            
         })
-         // console.log(data);
     }
+}
+
+function getFeedUrl(source_name){
+    return top_headlines;
 }
