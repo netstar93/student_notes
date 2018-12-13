0964ee6e89c6148c434251086d4af3612fe16ae2
netstar93 <nitishnick93@gmail.com>
2018-12-14 00:33:37 +0530

    resolve confloct

---
+++ models/students.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var common = require('../config/config');
+var config = common.config();
+mongoose.connect(config.database , {useNewUrlParser: true});
+var schema = new mongoose.Schema({
+    name : { type: String,min: 5 , max : 20 },
+	password : String,
+	email : String,
+	course: String,
+	year : Number,
+	image: String    
+},
+{
+	timestamps : true
+});
+
+// let timestampPlugin = require('../plugins/timestamp');
+// noteSchema.plugin(timestampPlugin);
+module.exports = mongoose.model('Students' , schema);
